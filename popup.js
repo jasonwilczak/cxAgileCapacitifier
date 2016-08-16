47c7fad868f9bc476eef209264e7f08647adabfd
Jason Wilczak <jason.wilczak@gmail.com>
2016-08-15 20:55:28 -0400

    finished logic

--- popup.js
+++ popup.js
@@ -1,17 +1,18 @@
 (function(){
+  var isLoaded = false;
   var viewModel = {
     iterationDays: getBasePropertyType('daysPerIteration'),
     workHours: getBasePropertyType('workHoursPerDay'),
     holidays: getBasePropertyType('holidaysThisIteration'),
     pto: getBasePropertyType('ptoThisIteration'),
-    allocation: getBasePropertyType('allocatoinPercentage'),
-    capacity: getBasePropertyType('capacityHours')
+    allocation: getBasePropertyType('allocationPercentage'),
+    capacity: getBasePropertyType('capacityResult')
   };
   function getBasePropertyType(elementId) {
     return {
       get: function () {
         var element = getElement(elementId);
-        return ParseInt(element.value);
+        return parseInt(element.value);
       },
       set: function (value) {
         var element = getElement(elementId);
@@ -24,19 +25,37 @@
     return document.getElementById(elementId);
   }
   function onCalculateClick() {
-    var workingIterationDays = (viewModel.iterationDays.get() - viewModel.holidays.get() - viewModel.pto.get());
+    var itDays = viewModel.iterationDays.get();
+    var holidays = viewModel.holidays.get();
+    var pto = viewModel.pto.get();
+    var workingIterationDays = (itDays-holidays-pto);
     workingIterationDays = workingIterationDays > 0 ? workingIterationDays : 0;
-    var workingHours = workingIterationDays * viewModel.workHours.get();
-    var percentageAvailable = (workingHours * viewModel.allocation.get()) / 100;
-    viewModel.capacity.set(percentageAvailable);
+    var workHours = viewModel.workHours.get();
+    var workingHours = workingIterationDays * workHours;
+    var allocation = viewModel.allocation.get();
+    var capacityAvailable = (workingHours * allocation) / 100;
+    viewModel.capacity.set(capacityAvailable+'hrs');
   }
+  function setStartingInputs() {
+    if(isLoaded) return;
+    //check localstorage
+    viewModel.iterationDays.set('10');
+    viewModel.holidays.set('0');
+    viewModel.pto.set('0');
+    viewModel.workHours.set('6');
+    viewModel.allocation.set('100');
+    onCalculateClick();
+    isLoaded = true;
+  }
+  function init() 
+  {
 document.addEventListener('DOMContentLoaded', function () {
-  
-  function init() {
+    setStartingInputs();
     var calculateButton = getElement('calculate');
     calculateButton.addEventListener('click', onCalculateClick, false);
+  
+}, false);
   }
   init();
-}, false);
 })();
 
